3770ae127226e37a14e3bf46c30c3c55771ce338
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-04 14:35:12 +0200

    chore: add eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express'
-const app = express()
-const port = 9000
+import express from "express";
+const app = express();
+const port = 9000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (_req, res) => res.send("Hello World!"));
 
-app.listen(port)
+app.listen(port);
 
-console.log(`[app]: http://localhost:${port}`)
+console.log(`[app]: http://localhost:${port}`);
