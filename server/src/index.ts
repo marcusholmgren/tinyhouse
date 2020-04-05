9d690e713ca913a8e1d8fb87af93e8d378666169
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-05 22:12:14 +0200

    feat: add /listings routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { listings } from "./listing";
 const app = express();
 const port = 9000;
 
-app.get("/", (_req, res) => res.send("Hello World!"));
+app.use(bodyParser.json());
+
+// /listings
+app.get("/listings", (_req, res) => {
+  return res.send(listings);
+});
+app.post("/listings", (req, res) => {
+  const id: string = req.body.id;
+
+  const index = listings.findIndex(x => x.id === id);
+  if (index > 0) {
+    return res.send(listings.splice(index, 1)[0]);
+  }
+  return res.status(404).send("Could not be found.");
+});
 
 app.listen(port);
 
