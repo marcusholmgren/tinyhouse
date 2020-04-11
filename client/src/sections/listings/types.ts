77e278f21b29f25f330c2415a726aadd3a5061dd
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-11 18:05:45 +0200

    refactor: add types for Listings api response

---
+++ client/src/sections/listings/types.ts
@@ -0,0 +1,15 @@
+interface Listing {
+    id: string;
+    title: string;
+    image: string;
+    address: string;
+    price: number;
+    numOfGuests: number;
+    numOfBeds: number;
+    numOfBaths: number;
+    rating: number;
+  }
+
+  export interface ListingsData {
+    listings: Listing[];
+  }
