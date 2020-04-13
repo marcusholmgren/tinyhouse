562d45276d00bf6fbd54d1b556f72c47f901225b
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-13 09:45:49 +0200

    feat: use React Apollo hooks.

--- client/src/sections/listings/listings.tsx
+++ client/src/sections/listings/listings.tsx
@@ -1,77 +1,101 @@
-import React from 'react'
-import { useQuery, useMutation } from '../../lib/api'
-import { DeleteListingsData, DeleteListingsVariables, ListingsData } from './types'
+import React from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import {
+  DeleteListingsData,
+  DeleteListingsVariables,
+  ListingsData,
+} from "./types";
 
-const LISTINGS = `query Listings {
-  listings {
-    id
-    title
-    image
-    address
-    price
-    numOfGuests
-    numOfBeds
-    numOfBaths
-    rating
+const LISTINGS = gql`
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
   }
-}`;
+`;
 
-const DELETE_LISTING = `mutation DeleteListing($id: ID!) {
-  deleteListing(id: $id) {
-    id
-    title
-    image
-    address
-    price
-    numOfBeds
-    numOfBaths
-    numOfGuests
-    rating
+const DELETE_LISTING = gql`
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+      title
+      image
+      address
+      price
+      numOfBeds
+      numOfBaths
+      numOfGuests
+      rating
+    }
   }
-}`;
-
+`;
 
 interface Props {
-    title: string
+  title: string;
 }
-
 
 export const Listings = ({ title }: Props) => {
-    const {data, loading, error, refresh} = useQuery<ListingsData>(LISTINGS);
-    const [deleteListing, {loading: deleteLoading, error: deleteError}] = useMutation<DeleteListingsData, DeleteListingsVariables>(DELETE_LISTING);
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
+  const [
+    deleteListing,
+    { loading: deleteLoading, error: deleteError },
+  ] = useMutation<DeleteListingsData, DeleteListingsVariables>(DELETE_LISTING);
 
+  const listings = data?.listings ?? null;
 
-    const listings = data?.listings ?? null;
+  const handleDeleteListing = async (id: string) => {
+    await deleteListing({ variables: { id } });
+    refetch();
+  };
 
+  const listingsList =
+    listings?.map((listing) => (
+      <li key={listing.id}>
+        {listing.title}
+        <button onClick={() => handleDeleteListing(listing.id)}>Delete</button>
+      </li>
+    )) ?? null;
 
-    const handleDeleteListing = async (id: string) => {
-        await deleteListing({id});
-        refresh();
-    }
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+  if (error) {
+    return (
+      <h2>
+        Oh no! Something went wrong - please try again later{" "}
+        <span role="img" aria-label="Sad face emoji">
+          😞
+        </span>
+      </h2>
+    );
+  }
 
-    const listingsList = listings?.map(listing =>
-        <li key={listing.id}>
-            {listing.title}
-            <button onClick={() => handleDeleteListing(listing.id)}>Delete</button>
-        </li>
-    ) ?? null;
-
-    if (loading) {
-        return <h2>loading...</h2>
-    }
-    if (error) {
-        return <h2>Oh no! Something went wrong - please try again later <span role="img" aria-label="Sad face emoji">😞</span></h2>
-    }
-
-    const deleteListingLoadingMessage = deleteLoading ? <h4>Deletion in progress...</h4> : null;
-    const deleteListingError = deleteError ? <h4>Oh no! Something went wrong - could not delete item <span role="img" aria-label="Sad face emoji">😞</span></h4> : null
-    return <div>
-        <h2>{title}</h2>
-        {deleteListingLoadingMessage}
-        <ul>
-            {listingsList}
-        </ul>
-        {deleteListingError
-        }
+  const deleteListingLoadingMessage = deleteLoading ? (
+    <h4>Deletion in progress...</h4>
+  ) : null;
+  const deleteListingError = deleteError ? (
+    <h4>
+      Oh no! Something went wrong - could not delete item{" "}
+      <span role="img" aria-label="Sad face emoji">
+        😞
+      </span>
+    </h4>
+  ) : null;
+  return (
+    <div>
+      <h2>{title}</h2>
+      {deleteListingLoadingMessage}
+      <ul>{listingsList}</ul>
+      {deleteListingError}
     </div>
-}
+  );
+};
