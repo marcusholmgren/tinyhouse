9b9646c48d129538916f01d5c2f953c21d7aa7b8
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-08-24 18:23:52 +0200

    feat: Database document structure

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,15 +1,56 @@
 import {ObjectId, Collection} from 'mongodb';
 
+export enum ListingType {
+    Apartment = "APARTMENT",
+    House = "HOUSE",
+}
+
+interface BookingsIndexMonth {
+    [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+    [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+    [key: string]: BookingsIndexYear;
+}
+
 export interface Booking {
     _id: ObjectId;
+    listing: ObjectId;
+    tenant: string;
+    checkIn: string;
+    checkOut: string;
 }
 
 export interface Listing {
     _id: ObjectId;
+    title: string;
+    description: string;
+    image: string;
+    host: string;
+    type: ListingType;
+    address: string;
+    country: string;
+    admin: string;
+    city: string;
+    bookings: ObjectId[];
+    bookingsIndex: BookingsIndex;
+    price: number;
 }
 
 export interface User {
-    _id: ObjectId;
+    _id: string;
+    token: string;
+    name: string;
+    avatar: string;
+    contact: string;
+    walletId?: string;
+    income: number;
+    bookings: ObjectId[];
+    listings: ObjectId[];
 }
 
 export interface Database {
