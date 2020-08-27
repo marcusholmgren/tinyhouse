957c36f5a4c0d5f23640794210d1eec3db3c54b2
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-08-27 15:15:34 +0200

    feat: built the authentication resolvers

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,17 +1,122 @@
-import { IResolvers } from 'apollo-server-express';
+import crypto from 'crypto';
+import {IResolvers} from 'apollo-server-express';
+import {Database, User, Viewer} from "../../../lib/types";
+import {Google} from "../../../lib/api";
+import {LogInArgs} from "./types";
+
+async function logInViaGoogle(code: string, token: string, db: Database): Promise<User | undefined> {
+    const {user} = await Google.logIn(code);
+
+    if (!user) {
+        throw new Error('Google log in error')
+    }
+
+    // Names/Photos/Email Lists
+    const userNamesList = user.names && user.names.length ? user.names : null;
+    const userPhotosList = user.photos && user.photos.length ? user.photos : null;
+    const userEmailsList =
+        user.emailAddresses && user.emailAddresses.length ? user.emailAddresses : null;
+
+    // User Display Name
+    const userName = userNamesList ? userNamesList[0].displayName : null;
+
+    // User Id
+    const userId =
+        userNamesList && userNamesList[0].metadata && userNamesList[0].metadata.source
+            ? userNamesList[0].metadata.source.id
+            : null;
+
+    // User Avatar
+    const userAvatar =
+        userPhotosList && userPhotosList[0].url ? userPhotosList[0].url : null;
+
+    // User Email
+    const userEmail =
+        userEmailsList && userEmailsList[0].value ? userEmailsList[0].value : null;
+
+    if (!userId || !userName || !userAvatar || !userEmail) {
+        throw new Error("Google login error");
+    }
+
+
+    const updateRes = await db.users.findOneAndUpdate(
+        {_id: userId},
+        {
+            $set: {
+                name: userName,
+                avatar: userAvatar,
+                contact: userEmail,
+                token: token
+            }
+        }, {returnOriginal: false})
+
+    let viewer = updateRes.value;
+
+    if (!viewer) {
+        const inserRes = await db.users.insertOne({
+            _id: userId,
+            name: userName,
+            avatar: userAvatar,
+            contact: userEmail,
+            token: token,
+            bookings: [],
+            income: 0,
+            listings: []
+        })
+
+        viewer = inserRes.ops[0];
+    }
+
+    return viewer;
+}
 
 export const viewResolvers: IResolvers = {
     Query: {
-        authUrl: () => {
-            return "Query.authUrl"
+        authUrl: (): string => {
+            try {
+                return Google.authUrl;
+            } catch (error) {
+                throw new Error(`Failed to query Google Auth Url: ${error}`)
+            }
         }
     },
     Mutation: {
-        logIn: () => {
-            return "Mutation.logIn"
+        logIn: async (_root: undefined, {input}: LogInArgs, {db}: { db: Database }): Promise<Viewer> => {
+            try {
+                const code = input ? input.code : null;
+                const token = crypto.randomBytes(16).toString('hex');
+
+                const viewer: User | undefined = code ? await logInViaGoogle(code, token, db) : undefined;
+
+                if (!viewer) {
+                    return {didRequest: true}
+                }
+
+                return {
+                    _id: viewer._id,
+                    token: viewer.token,
+                    avatar: viewer.avatar,
+                    walletId: viewer.walletId,
+                    didRequest: true
+                }
+            } catch (error) {
+                throw new Error(`Failed to log in: ${error}`)
+            }
         },
-        logOut: () => {
-            return "Mutation.logOut"
+        logOut: (): Viewer => {
+            try {
+                return { didRequest: true}
+            } catch (error) {
+                throw new Error(`Failed to log out: ${error}`)
+            }
+        }
+    },
+    Viewer: {
+        id: (viewer: Viewer): string | undefined => {
+            return viewer._id
+        },
+        hasWallet: (viewer: Viewer): boolean | undefined => {
+            return viewer.walletId ? true : undefined
         }
     }
 }
--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,5 +1,13 @@
 import {ObjectId, Collection} from 'mongodb';
 
+export interface Viewer {
+    _id?: string;
+    token?: string;
+    avatar?: string;
+    walletId?: string;
+    didRequest: boolean
+}
+
 export enum ListingType {
     Apartment = "APARTMENT",
     House = "HOUSE",
