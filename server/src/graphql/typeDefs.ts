faaeb56e5ee9f5a0b75f8fcf436d1ece3ae8046d
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-08-31 13:59:45 +0200

    feat: ad graphql type definitions for user query

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,6 +1,55 @@
 import {gql} from "apollo-server-express";
 
 export const typeDefs = gql`
+  type Listings {
+    total: Int!
+    result: [Listing!]!
+  }
+  
+  enum ListingType {
+    APARTMENT
+    HOUSE
+  }
+  
+  type Listing {
+    id: ID!
+    title: String!
+    description: String!
+    image: String!
+    host: User!
+    type: ListingType!
+    address: String!
+    city: String!
+    bookings(limit: Int!, page: Int!): Bookings
+    bookingsIndex: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+  
+  type Bookings {
+    total: Int!
+    result: [Booking!]!
+  }
+  
+  type Booking {
+    id: ID!
+    listing: Listing!
+    tenant: User!
+    checkIn: String!
+    checkOut: String!
+  }
+  
+  type User {
+    id: ID!
+    name: String!
+    avatar: String!
+    contact: String!
+    hasWallet: Boolean!
+    income: Int
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+  }
+  
   type Viewer {
     id: ID
     token: String
@@ -15,7 +64,7 @@ export const typeDefs = gql`
 
   type Query {
     authUrl: String!
-    user: String!
+    user(id: ID!): User!
   }
   
   type Mutation {
